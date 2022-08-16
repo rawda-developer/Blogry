3ee794b08badcd19b8f5440f6ec6c4df709cea38
Rawda Yasser <rawda.developer@gmail.com>
2022-08-17 01:57:30 +0200

    Feat: add NavBar component as a horizontal menu for all the routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AboutPage from './pages/AboutPage';
 import HomePage from './pages/HomePage';
 import ArticlePage from './pages/ArticlePage';
 import ArticleList from './pages/ArticlesList';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <>
       <Router>
+        <NavBar />
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/about' element={<AboutPage />} />
---
+++ frontend/src/NavBar.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+const NavBar = () => (
+  <nav>
+    <ul className='menu menu-vertical lg:menu-horizontal bg-base-100 rounded-box'>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to='/about'>About</Link>
+      </li>
+      <li>
+        <Link to='/article'>Article</Link>
+      </li>
+      <li>
+        <Link to='/articles'>Articles</Link>
+      </li>
+    </ul>
+  </nav>
+);
+export default NavBar;
