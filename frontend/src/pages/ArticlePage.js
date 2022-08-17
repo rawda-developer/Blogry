65a381525ce526d2856a852c0cf4ea9ce85631ea
Rawda Yasser <rawda.developer@gmail.com>
2022-08-17 02:38:22 +0200

    Feat: add articles content and display the article in ArticlePage component based on the name in the parameter

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,9 +1,18 @@
 import { useParams } from 'react-router-dom';
+import articles from './ArticlesContent';
 const ArticlePage = () => {
   const { name } = useParams();
+  const article = articles.find((article) => article.name === name);
   return (
     <>
-      <h1 className='text-xl mb-5'>This is the {name} Article</h1>
+      <h1 className='text-2xl mb-5'>{article.title}</h1>
+      {article.content.map((paragraph, i) => (
+        <>
+          <p key={i}>{paragraph}</p>
+
+          <br />
+        </>
+      ))}
     </>
   );
 };
